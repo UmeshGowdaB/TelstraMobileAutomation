76c897f0db9a8d85accbdaaace4224651435ccaf
Umesh Bylappa <Umesh.Bylappa@travelctm.com>
2020-04-05 16:24:33 +1000

    helper class

---
+++ src/test/java/helpers/appHelper.java
@@ -0,0 +1,27 @@
+package helpers;
+
+import org.openqa.selenium.interactions.Interaction;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
+
+import java.time.Duration;
+
+public class appHelper {
+
+    public Sequence scrollDown() {
+
+        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+        Interaction moveToStart = finger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), 520, 1530);
+        Interaction pressDown = finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg());
+        Interaction moveToEnd = finger.createPointerMove(Duration.ofMillis(1000), PointerInput.Origin.viewport(), 520, 490);
+        Interaction pressUp = finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg());
+
+        Sequence swipe = new Sequence(finger, 0);
+        swipe.addAction(moveToStart);
+        swipe.addAction(pressDown);
+        swipe.addAction(moveToEnd);
+        swipe.addAction(pressUp);
+        return swipe;
+    }
+
+}
--- src/test/java/pages/searchPage.java
+++ src/test/java/pages/searchPage.java
@@ -2,15 +2,26 @@ package pages;
 
 import common.baseDriver;
 import io.appium.java_client.MobileBy;
+import jdk.internal.vm.compiler.word.Pointer;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Interaction;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.lang.reflect.Array;
+import java.time.Duration;
+import java.util.Arrays;
+import helpers.appHelper;
 
 public class searchPage {
 
     private ExpectedConditions ExceptedConditions;
     baseDriver driver = new baseDriver();
+    appHelper helper = new appHelper();
 
-    private String searchBox = "id/rs_search_src_text";
+    private By searchBox = MobileBy.AccessibilityId("id/rs_search_src_text");
 
 
     public void searchItem(String itemName) {
@@ -24,6 +35,9 @@ public class searchPage {
     }
 
     public void selectItem() {
+
+        driver.androidDriver().perform(Arrays.asList(helper.scrollDown()));
+        driver.androidDriver()
 
     }
 
